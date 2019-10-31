84e82b94287ae517eb687aef9f50abe8be798306
KyleHorton <kyle.horton@quinnipiac.edu>
2019-10-31 15:16:12 -0400

    leagues

--- .vscode/loadData.py
+++ .vscode/loadData.py
@@ -1,22 +1,22 @@
 import csv
-import pymysql.cursors
+import pymysql
 
 def make_connection():
     return pymysql.connect(
-        host='reds-lineup.c63ijpxfdwey.us-east-1.rds.amazonaws.com',
+        host='reds-offense.c63ijpxfdwey.us-east-1.rds.amazonaws.com',
         user='admin', 
-        passwd='baseball',
-        db='reds-offense',
-        port=3306)
+        password='baseball',
+        port=3306,
+        autocommit=True)
 
 connect = make_connection()
 database = connect.cursor()
 
 # setting up DB Schema
 
-database.execute('DROP DATABASE IF EXISTS reds-offense');
-database.execute('CREATE DATABASE reds-offense');
-database.execute('USE reds-offense');
+database.execute('DROP DATABASE IF EXISTS RedsOffense');
+database.execute('CREATE DATABASE RedsOffense');
+database.execute('USE RedsOffense');
 
 database.execute('DROP TABLE IF EXISTS League');
 database.execute('DROP TABLE IF EXISTS Reds');
@@ -28,7 +28,7 @@ database.execute('DROP TABLE IF EXISTS Shortstops');
 database.execute('DROP TABLE IF EXISTS Outfielders');
 
 database.execute('''CREATE TABLE League (
-    Rank     INT NOT NULL,
+    Rank     INT NOT NULL AUTO_INCREMENT,
     Team     VARCHAR(12) NOT NULL,
     G        INT NOT NULL,
     PA       INT NOT NULL,
@@ -36,13 +36,13 @@ database.execute('''CREATE TABLE League (
     R        INT NOT NULL,
     RBI      INT NOT NULL,
     SB       INT NOT NULL,
-    BB%      DECIMAL NOT NULL,
-    K%       DECIMAL NOT NULL,
-    AVG      DECIMAL NOT NULL,
-    OBP      DECIMAL NOT NULL,
-    SLG      DECIMAL NOT NULL,
-    wOBA     DECIMAL NOT NULL,
-    wRC+     INT NOT NULL,
+    BB      DECIMAL(5,3) NOT NULL,
+    K       DECIMAL(5,3) NOT NULL,
+    AVG      DECIMAL(5,3) NOT NULL,
+    OBP      DECIMAL(5,3) NOT NULL,
+    SLG      DECIMAL(5,3) NOT NULL,
+    wOBA     DECIMAL(5,3) NOT NULL,
+    wRC   INT NOT NULL,
     PRIMARY KEY (Rank)
 )''');
 
@@ -56,13 +56,13 @@ database.execute('''CREATE TABLE Reds (
     R         INT NOT NULL,
     RBI       INT NOT NULL,
     SB        INT NOT NULL,
-    BB%       DECIMAL NOT NULL,
-    K%        DECIMAL NOT NULL,
-    AVG       DECIMAL NOT NULL,
-    OBP       DECIMAL NOT NULL,
-    SLG       DECIMAL NOT NULL,
-    wOBA      DECIMAL NOT NULL,
-    wRC+      INT NOT NULL,
+    BB       DECIMAL(5,3) NOT NULL,
+    K        DECIMAL(5,3) NOT NULL,
+    AVG       DECIMAL(5,3) NOT NULL,
+    OBP       DECIMAL(5,3) NOT NULL,
+    SLG       DECIMAL(5,3) NOT NULL,
+    wOBA      DECIMAL(5,3) NOT NULL,
+    wRC     INT NOT NULL,
     PRIMARY KEY (Rank)
 )''');
 
@@ -76,13 +76,13 @@ database.execute('''CREATE TABLE Catchers (
     R        INT NOT NULL,
     RBI      INT NOT NULL,
     SB       INT NOT NULL,
-    BB%      DECIMAL NOT NULL,
-    K%       DECIMAL NOT NULL,
-    AVG      DECIMAL NOT NULL,
-    OBP      DECIMAL NOT NULL,
-    SLG      DECIMAL NOT NULL,
-    wOBA     DECIMAL NOT NULL,
-    wRC+     INT NOT NULL,
+    BB      DECIMAL(5,3) NOT NULL,
+    K       DECIMAL(5,3) NOT NULL,
+    AVG      DECIMAL(5,3) NOT NULL,
+    OBP      DECIMAL(5,3) NOT NULL,
+    SLG      DECIMAL(5,3) NOT NULL,
+    wOBA     DECIMAL(5,3) NOT NULL,
+    wRC    INT NOT NULL,
     PRIMARY KEY (Rank)
 )''');
 
@@ -96,13 +96,13 @@ database.execute('''CREATE TABLE FirstBasemen (
     R        INT NOT NULL,
     RBI      INT NOT NULL,
     SB       INT NOT NULL,
-    BB%      DECIMAL NOT NULL,
-    K%       DECIMAL NOT NULL,
-    AVG      DECIMAL NOT NULL,
-    OBP      DECIMAL NOT NULL,
-    SLG      DECIMAL NOT NULL,
-    wOBA     DECIMAL NOT NULL,
-    wRC+     INT NOT NULL,
+    BB      DECIMAL(5,3) NOT NULL,
+    K       DECIMAL(5,3) NOT NULL,
+    AVG      DECIMAL(5,3) NOT NULL,
+    OBP      DECIMAL(5,3) NOT NULL,
+    SLG      DECIMAL(5,3) NOT NULL,
+    wOBA     DECIMAL(5,3) NOT NULL,
+    wRC    INT NOT NULL,
     PRIMARY KEY (Rank)
 )''');
 
@@ -116,13 +116,13 @@ database.execute('''CREATE TABLE SecondBasemen (
     R        INT NOT NULL,
     RBI      INT NOT NULL,
     SB       INT NOT NULL,
-    BB%      DECIMAL NOT NULL,
-    K%       DECIMAL NOT NULL,
-    AVG      DECIMAL NOT NULL,
-    OBP      DECIMAL NOT NULL,
-    SLG      DECIMAL NOT NULL,
-    wOBA     DECIMAL NOT NULL,
-    wRC+     INT NOT NULL,
+    BB      DECIMAL(5,3) NOT NULL,
+    K       DECIMAL(5,3) NOT NULL,
+    AVG      DECIMAL(5,3) NOT NULL,
+    OBP      DECIMAL(5,3) NOT NULL,
+    SLG      DECIMAL(5,3) NOT NULL,
+    wOBA     DECIMAL(5,3) NOT NULL,
+    wRC    INT NOT NULL,
     PRIMARY KEY (Rank)
 )''');
 
@@ -136,13 +136,13 @@ database.execute('''CREATE TABLE ThirdBasemen (
     R        INT NOT NULL,
     RBI      INT NOT NULL,
     SB       INT NOT NULL,
-    BB%      DECIMAL NOT NULL,
-    K%       DECIMAL NOT NULL,
-    AVG      DECIMAL NOT NULL,
-    OBP      DECIMAL NOT NULL,
-    SLG      DECIMAL NOT NULL,
-    wOBA     DECIMAL NOT NULL,
-    wRC+     INT NOT NULL,
+    BB      DECIMAL(5,3) NOT NULL,
+    K       DECIMAL(5,3) NOT NULL,
+    AVG      DECIMAL(5,3) NOT NULL,
+    OBP      DECIMAL(5,3) NOT NULL,
+    SLG      DECIMAL(5,3) NOT NULL,
+    wOBA     DECIMAL(5,3) NOT NULL,
+    wRC    INT NOT NULL,
     PRIMARY KEY (Rank)
 )''');
 
@@ -156,13 +156,13 @@ database.execute('''CREATE TABLE Shortstops (
     R        INT NOT NULL,
     RBI      INT NOT NULL,
     SB       INT NOT NULL,
-    BB%      DECIMAL NOT NULL,
-    K%       DECIMAL NOT NULL,
-    AVG      DECIMAL NOT NULL,
-    OBP      DECIMAL NOT NULL,
-    SLG      DECIMAL NOT NULL,
-    wOBA     DECIMAL NOT NULL,
-    wRC+     INT NOT NULL,
+    BB      DECIMAL(5,3) NOT NULL,
+    K       DECIMAL(5,3) NOT NULL,
+    AVG      DECIMAL(5,3) NOT NULL,
+    OBP      DECIMAL(5,3) NOT NULL,
+    SLG      DECIMAL(5,3) NOT NULL,
+    wOBA     DECIMAL(5,3) NOT NULL,
+    wRC    INT NOT NULL,
     PRIMARY KEY (Rank)
 )''');
 
@@ -176,26 +176,26 @@ database.execute('''CREATE TABLE Outfielders (
     R        INT NOT NULL,
     RBI      INT NOT NULL,
     SB       INT NOT NULL,
-    BB%      DECIMAL NOT NULL,
-    K%       DECIMAL NOT NULL,
-    AVG      DECIMAL NOT NULL,
-    OBP      DECIMAL NOT NULL,
-    SLG      DECIMAL NOT NULL,
-    wOBA     DECIMAL NOT NULL,
-    wRC+     INT NOT NULL,
+    BB      DECIMAL(5,3) NOT NULL,
+    K       DECIMAL(5,3) NOT NULL,
+    AVG      DECIMAL(5,3) NOT NULL,
+    OBP      DECIMAL(5,3) NOT NULL,
+    SLG      DECIMAL(5,3) NOT NULL,
+    wOBA     DECIMAL(5,3) NOT NULL,
+    wRC    INT NOT NULL,
     PRIMARY KEY (Rank)
 )''');
 
-file_League = open("../League.csv", "r")
-file_Reds = open("../Reds.csv", "r")
-file_Catchers = open("../Catchers.csv", "r")
-file_FirstBasemen = open("../FirstBasemen.csv", "r")
-file_SecondBasemen = open("../SecondBasemen.csv", "r")
-file_ThirdBasemen = open("../ThirdBasemen.csv", "r")
-file_Shortstops = open("../Shortstops.csv", "r")
-file_Outfielders = open("../Outfielders.csv", "r")
+file_League = open("League.csv", "r")
+file_Reds = open("Reds.csv", "r")
+file_Catchers = open("Catchers.csv", "r")
+file_FirstBasemen = open("FirstBasemen.csv", "r")
+file_SecondBasemen = open("SecondBasemen.csv", "r")
+file_ThirdBasemen = open("ThirdBasemen.csv", "r")
+file_Shortstops = open("Shortstops.csv", "r")
+file_Outfielders = open("Outfielders.csv", "r")
 
-leagueData = file_League.readlines()
+leagueData = csv.reader(file_League, delimiter=',')
 redsData = file_Reds.readlines()
 catchersData = file_Catchers.readlines()
 firstBasemenData = file_FirstBasemen.readlines()
@@ -204,9 +204,31 @@ thirdBasemenData = file_ThirdBasemen.readlines()
 shortstopsBasemenData = file_Shortstops.readlines()
 outfieldersData = file_Outfielders.readlines()
 
-for lines in leagueData:
-    print(lines)
 
-database.close()
+for lines in leagueData:
+    team = lines[0]
+    G = lines[1]
+    PA = lines[2]
+    HR = lines[3]
+    R = lines[4]
+    RBI = lines[5]
+    SB = lines[6]
+    BB = lines[7]
+    K = lines[8]
+    AVG = lines[9]
+    OBP = lines[10]
+    SLG = lines[11]
+    wOBA = lines[12]
+    wRC = lines[13]
+
+    sql = "INSERT INTO League (Team, G, PA, HR, R, RBI, SB, BB, K, AVG, OBP, SLG, wOBA, wRC) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    vals = (team, G, PA, HR, R, RBI, SB, BB, K, AVG, OBP, SLG, wOBA, wRC)
+
+    database.execute(sql, vals)
+
+
+database.close();
+connect.commit();
+connect.close();
 
 
